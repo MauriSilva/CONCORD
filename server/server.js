e797125e3f80970689c906effa33a010449d416f
Mauricio F <mauricioferrazsilva9@gmai.com>
2026-08-27 15:55:02 -0300

    corrigindo CORS

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*"
+        origin: "*",
+        methods: ["GET", "POST"]
     }
 });
 
